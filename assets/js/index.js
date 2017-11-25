0588b6a2089e8120d7fc6408f60b808c7dab5e77
octo-pod <33822232+octo-pod@users.noreply.github.com>
2017-11-25 12:06:29 -0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,3 @@
-function toggleItem(elem) {
-  for (var i = 0; i < elem.length; i++) {
-    elem[i].addEventListener("click", function(e) {
-      var current = this;
-      for (var i = 0; i < elem.length; i++) {
-        if (current != elem[i]) {
-          elem[i].classList.remove('blank');
-        } else if (current.classList.contains('blank') === true) {
-          current.classList.remove('blank');
-        } else {
-          current.classList.add('blank')
-        }
-      }
-      e.preventDefault();
-    });
-  };
+document.getElementById("click").onclick = function() {
+    document.getElementById("wrapper").remove();
 }
-toggleItem(document.querySelectorAll('.wrapper'));
