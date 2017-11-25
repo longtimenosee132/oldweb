2875ba4a4c93da31b2d2e8ff1bec3bd80170bd6d
octo-pod <33822232+octo-pod@users.noreply.github.com>
2017-11-25 12:18:33 -0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,1 @@
-(function() {
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-    link.type = 'image/x-icon';
-    link.rel = 'shortcut icon';
-    link.href = 'lib/img/octopod2.png';
-    document.getElementsByTagName('head')[0].appendChild(link);
-})();
+
